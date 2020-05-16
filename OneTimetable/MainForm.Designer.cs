7b06d6b84978481fcdc0f6fc48c7291da25894cf
WY <wy1205691775@outlook.com>
2020-05-16 20:55:32 +0800

    new in 2020.5.16

---
+++ OneTimetable/MainForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace OneTimetable
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.ButtomInfo = new System.Windows.Forms.PictureBox();
+            this.ButtomSettings = new System.Windows.Forms.PictureBox();
+            this.ButtomExit = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtomInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtomSettings)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtomExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Controls.Add(this.label4);
+            this.flowLayoutPanel1.Controls.Add(this.label5);
+            this.flowLayoutPanel1.Controls.Add(this.label6);
+            this.flowLayoutPanel1.Controls.Add(this.label7);
+            this.flowLayoutPanel1.Controls.Add(this.label8);
+            this.flowLayoutPanel1.Controls.Add(this.label9);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(35, 102);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(159, 702);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 36.30252F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 78);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "语文";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 36.30252F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(3, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 78);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "语文";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 36.30252F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(3, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 78);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "语文";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 36.30252F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(3, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 78);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "语文";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 36.30252F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(3, 312);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 78);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "语文";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 36.30252F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(3, 390);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 78);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "语文";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 36.30252F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(3, 468);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(153, 78);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "语文";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微软雅黑", 36.30252F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(3, 546);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(153, 78);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "语文";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("微软雅黑", 36.30252F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.Location = new System.Drawing.Point(3, 624);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(153, 78);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "语文";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.Controls.Add(this.ButtomInfo);
+            this.flowLayoutPanel2.Controls.Add(this.ButtomSettings);
+            this.flowLayoutPanel2.Controls.Add(this.ButtomExit);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(210, 70);
+            this.flowLayoutPanel2.TabIndex = 7;
+            // 
+            // ButtomInfo
+            // 
+            this.ButtomInfo.BackColor = System.Drawing.SystemColors.Control;
+            this.ButtomInfo.Image = global::OneTimetable.Properties.Resources.icons8_info_popup_96;
+            this.ButtomInfo.Location = new System.Drawing.Point(3, 3);
+            this.ButtomInfo.Name = "ButtomInfo";
+            this.ButtomInfo.Size = new System.Drawing.Size(64, 64);
+            this.ButtomInfo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ButtomInfo.TabIndex = 6;
+            this.ButtomInfo.TabStop = false;
+            this.ButtomInfo.Click += new System.EventHandler(this.ButtomInfo_Click);
+            // 
+            // ButtomSettings
+            // 
+            this.ButtomSettings.Image = global::OneTimetable.Properties.Resources.icons8_adjust_96;
+            this.ButtomSettings.Location = new System.Drawing.Point(73, 3);
+            this.ButtomSettings.Name = "ButtomSettings";
+            this.ButtomSettings.Size = new System.Drawing.Size(64, 64);
+            this.ButtomSettings.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ButtomSettings.TabIndex = 7;
+            this.ButtomSettings.TabStop = false;
+            // 
+            // ButtomExit
+            // 
+            this.ButtomExit.Image = global::OneTimetable.Properties.Resources.icons8_exit_96;
+            this.ButtomExit.Location = new System.Drawing.Point(143, 3);
+            this.ButtomExit.Name = "ButtomExit";
+            this.ButtomExit.Size = new System.Drawing.Size(64, 64);
+            this.ButtomExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ButtomExit.TabIndex = 8;
+            this.ButtomExit.TabStop = false;
+            this.ButtomExit.Click += new System.EventHandler(this.ButtomExit_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(119F, 119F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(233, 827);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "一个课表";
+            this.Activated += new System.EventHandler(this.MainForm_Activated);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ButtomInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtomSettings)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtomExit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox ButtomInfo;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.PictureBox ButtomSettings;
+        private System.Windows.Forms.PictureBox ButtomExit;
+    }
+}
+
---
+++ OneTimetable/MainForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OneTimetable
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+#if !DEBUG
+            SetToDeskTop();
+#endif
+            PictureBoxColor();
+        }
+        private void PictureBoxColor()
+        {
+            for (int i = 0; i < flowLayoutPanel2.Controls.Count; i++)
+            {
+                if (flowLayoutPanel2.Controls[i].GetType() == typeof(PictureBox))
+                {
+                    flowLayoutPanel2.Controls[i].MouseEnter += new System.EventHandler(pictureBox_MouseEnter);
+                    flowLayoutPanel2.Controls[i].MouseLeave += new System.EventHandler(pictureBox_MouseLeave);
+
+                }
+            }
+        }
+
+
+        #region 将窗体钉在桌面上
+        private void SetToDeskTop()
+        {
+            try
+            {
+                if (Environment.OSVersion.Version.Major < 6)
+                {
+                    base.SendToBack();
+                    IntPtr hWndNewParent = Win32.FindWindow("Progman", null);
+                    Win32.SetParent(base.Handle, hWndNewParent);
+                }
+                else
+                {
+                    IntPtr desktopHwnd = GetDesktopPtr();
+                    IntPtr ownHwnd = base.Handle;
+                    IntPtr result = Win32.SetParent(ownHwnd, desktopHwnd);
+
+                }
+
+            }
+            catch (ApplicationException exx)
+            {
+                MessageBox.Show(this, exx.Message, "Pin to Desktop");
+            }
+        }
+
+        private IntPtr GetDesktopPtr()
+        {
+            //http://blog.csdn.net/mkdym/article/details/7018318
+            // 情况一
+            IntPtr hwndWorkerW = IntPtr.Zero;
+            IntPtr hShellDefView = IntPtr.Zero;
+            IntPtr hwndDesktop = IntPtr.Zero;
+            IntPtr hProgMan = Win32.FindWindow("ProgMan", null);
+            if (hProgMan != IntPtr.Zero)
+            {
+                hShellDefView = Win32.FindWindowEx(hProgMan, IntPtr.Zero, "SHELLDLL_DefView", null);
+                if (hShellDefView != IntPtr.Zero)
+                {
+                    hwndDesktop = Win32.FindWindowEx(hShellDefView, IntPtr.Zero, "SysListView32", null);
+                }
+            }
+            if (hwndDesktop != IntPtr.Zero) return hwndDesktop;
+
+            // 情况二
+            while (hwndDesktop == IntPtr.Zero)
+            {//必须存在桌面窗口层次  
+                hwndWorkerW = Win32.FindWindowEx(IntPtr.Zero, hwndWorkerW, "WorkerW", null);//获得WorkerW类的窗口  
+                if (hwndWorkerW == IntPtr.Zero) break;//未知错误
+                hShellDefView = Win32.FindWindowEx(hwndWorkerW, IntPtr.Zero, "SysListView32", null);
+                if (hShellDefView == IntPtr.Zero) continue;
+                hwndDesktop = Win32.FindWindowEx(hShellDefView, IntPtr.Zero, "SysListView32", null);
+            }
+            return hwndDesktop;
+        }
+
+
+        private void MainForm_Activated(object sender, EventArgs e)
+        {
+#if DEBUG
+            return;
+#endif
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                Win32.SetWindowPos(base.Handle, 1, 0, 0, 0, 0, Win32.SE_SHUTDOWN_PRIVILEGE);
+            }
+
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+#if DEBUG
+            return;
+#endif
+
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                Win32.SetWindowPos(base.Handle, 1, 0, 0, 0, 0, Win32.SE_SHUTDOWN_PRIVILEGE);
+            }
+        }
+        #endregion
+
+        //窗口移动
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            Win32.ReleaseCapture();
+            Win32.SendMessage(this.Handle, Win32.WM_SYSCOMMAND, Win32.SC_MOVE + Win32.HTCAPTION, 0);//窗体移动
+
+        }
+
+        private void pictureBox_MouseEnter(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = sender as PictureBox;
+            pictureBox.BackColor = System.Drawing.SystemColors.ControlDark;
+        }
+        private void pictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = sender as PictureBox;
+            pictureBox.BackColor = System.Drawing.SystemColors.Control;
+        }
+
+        private void ButtomExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ButtomInfo_Click(object sender, EventArgs e)
+        {
+            string text = @"
+Made by Kelatte
+开源在Github
+";
+            MessageBox.Show(text);
+        }
+    }
+}
